3e39904a09674072e41a34d5ce028e16f1c64dc9
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-10 18:26:06 -0400

    setup typeorm connection infrastructure

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,21 @@ import errorMiddleware from '@middlewares/errorMiddleware';
 import { IndexRoutes } from '@routes/index.route';
 import { Logger } from '@utils/logger';
 import { catchAllMiddleware } from '@middlewares/catchAllMiddleware';
+import compression from 'compression';
+import { createConnection } from 'typeorm';
 class App {
   public app: express.Application;
-  public port: string | number = 3000;
-  // public env: string;
+  public port: string | number;
+  public env: string;
+  public dbConnectionType: string;
 
   public constructor() {
     this.app = express();
-    // this.port = process.env.PORT || 3000;
-    // this.env = process.env.NODE_ENV || 'development';
+    this.port = process.env.PORT || 3000;
+    this.env = process.env.NODE_ENV || 'development';
+    this.dbConnectionType = process.env.TYPEORM_CONNECTION_TYPE || 'development';
 
-    // this.env !== 'test' && this.connectToDatabase();
+    this.env !== 'testing' && this.connectToDatabase();
     this.initializeMiddlewares();
     this.initializeRoutes();
     this.initializeErrorHandling();
@@ -26,17 +30,19 @@ class App {
   public listen() {
     this.app.listen(this.port, () => {
       Logger.info(`=================================`);
-      // Logger.info(`======= ENV: ${this.env} =======`);
+      Logger.info(`======= ENV: ${this.env} =======`);
+      Logger.info(`======= DB: ${this.dbConnectionType} =======`);
       Logger.info(`🚀 App listening on port ${this.port}`);
       Logger.info(`=================================`);
     });
   }
 
-  // private connectToDatabase() {
-  //   createConnection(dbConnection);
-  // }
+  private connectToDatabase() {
+    createConnection(`${this.dbConnectionType}`);
+  }
 
   private initializeMiddlewares() {
+    this.app.use(compression());
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
