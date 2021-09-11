fb7756c608009dab703c888094645bc2b402cc42
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-10 20:50:33 -0400

    added user service methods

---
+++ src/services/user.service.ts
@@ -0,0 +1,22 @@
+export class UserService {
+  /**
+   * Retrieve users from db
+   *
+   * Valid query parameters:
+   * ?role=(ADMIN/USER)
+   * ?orderBy=<userField>:<ASC|DESC>
+   * ?skip=(0)
+   * ?take=(10)
+   */
+  findAllUsers() {}
+
+  findOneById() {}
+
+  findUserByEmail() {}
+
+  createUser() {}
+
+  updateUser() {}
+
+  deleteUser() {}
+}
